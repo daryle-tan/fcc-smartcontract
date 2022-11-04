3d6f66ffa8c9dff3ac313c09c70517c9e70a4b99
daryle <daryle.tan@gmail.com>
2022-11-04 09:50:46 -0500

    fin lesson 9

--- hardhat-smartcontract-lottery-fcc/helper-hardhat-config.js
+++ hardhat-smartcontract-lottery-fcc/helper-hardhat-config.js
@@ -15,7 +15,7 @@ const networkConfig = {
     },
     5: {
         name: "goerli",
-        subscriptionId: "6926",
+        subscriptionId: "6075",
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.1 ETH
